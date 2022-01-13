8e69fae39503a0c38cd1f956eaab28204c2d21d7
Blaz <blazyy@gmail.com>
2022-01-13 15:11:11 +0100

    added db.json;  props and foor loop showing Posts

--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -1,12 +1,48 @@
 <template>
-  <div class="home"></div>
+  <div class="home">
+    <h1>home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>
+      <h3>Loading...</h3>
+    </div>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
 
+import { ref } from "vue";
+import PostList from "../components/PostList.vue";
+
 export default {
   name: "Home",
-  components: {},
+  components: { PostList },
+
+  setup() {
+    const posts = ref([]);
+    const error = ref(null);
+
+    const load = async () => {
+      try {
+        let data = await fetch("http://localhost:3000/posts");
+        if (!data.ok) {
+          throw Error("no data available!");
+        }
+        posts.value = await data.json();
+      } catch (err) {
+        error.value = err.message;
+        console.log(err.value);
+      }
+    };
+
+    load();
+
+    return { posts, error };
+  },
 };
+
+// butto delete post
 </script>
