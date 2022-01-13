a9943f10f59cdcb20f05b7275b0a177aa7d490a5
Blaz <blazyy@gmail.com>
2022-01-13 15:43:02 +0100

    created composable functions; created details.vue

---
+++ dojo-blog/src/views/Details.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div v-if="error">
+      {{ error }}
+    </div>
+    <div v-if="post" class="post">
+      <h3>{{ post.title }}</h3>
+      <p class="pre">{{ post.body }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+import getPost from "../composables/getPost";
+
+export default {
+  name: "Details",
+  props: ["id"],
+  setup(props) {
+    const { post, error, load } = getPost(props.id);
+
+    load();
+    return { post, error };
+  },
+};
+</script>
+
+<style></style>
--- dojo-blog/src/views/Home.vue
+++ dojo-blog/src/views/Home.vue
@@ -14,30 +14,15 @@
 <script>
 // @ is an alias to /src
 
-import { ref } from "vue";
 import PostList from "../components/PostList.vue";
+import getPosts from "../composables/getPosts";
 
 export default {
   name: "Home",
   components: { PostList },
 
   setup() {
-    const posts = ref([]);
-    const error = ref(null);
-
-    const load = async () => {
-      try {
-        let data = await fetch("http://localhost:3000/posts");
-        if (!data.ok) {
-          throw Error("no data available!");
-        }
-        posts.value = await data.json();
-      } catch (err) {
-        error.value = err.message;
-        console.log(err.value);
-      }
-    };
-
+    const { posts, error, load } = getPosts();
     load();
 
     return { posts, error };
